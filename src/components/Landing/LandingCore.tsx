5583b0350785b655addf0e555daff165b2edb7c0
Louis <louis.dutoitld@gmail.com>
2022-06-02 19:12:22 +0200

    display svg

--- src/components/Landing/LandingCore.tsx
+++ src/components/Landing/LandingCore.tsx
@@ -1,8 +1,12 @@
+import Chart from "../../Chart.svg"
+
 export const LandingCore = () => {
     return (
         <img
-            src="Chart.svg"
+            src={Chart}
             alt="Stonks"
+            width="700"
+            height="700"
         />
     );
 };
