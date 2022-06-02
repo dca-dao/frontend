02730af5328f273d2acb47518d31fdc2f702a362
Louis <louis.dutoitld@gmail.com>
2022-06-02 16:46:01 +0200

    landing page WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import { ChainId, DAppProvider } from "@usedapp/core"
-import { Metamask } from "./components/Metamask"
+import { Metamask } from "./components/DCA/Metamask"
+import { ResponsiveAppBar } from "./components/Landing/AppBar"
 
 function App() {
   return (
-    <DAppProvider config={{
-      supportedChains: [ChainId.Kovan],
-      notifications: {
-        expirationPeriod: 1000,
-        checkInterval: 1000
-      }
-    }}>
-      <Metamask />
-    </DAppProvider>
+
+    // <DAppProvider config={{
+    //   supportedChains: [ChainId.Kovan],
+    //   notifications: {
+    //     expirationPeriod: 1000,
+    //     checkInterval: 1000
+    //   }
+    // }}>
+    //   <Metamask />
+    // </DAppProvider>
+    <ResponsiveAppBar />
   )
 }
 
---
+++ src/components/Landing/AppBar.tsx
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import LocalAtmIcon from '@mui/icons-material/LocalAtm';
+
+export const ResponsiveAppBar = () => {
+
+    return (
+        <AppBar position="static">
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <LocalAtmIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="a"
+                        href="/"
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'none', md: 'flex' },
+                            flexGrow: 1,
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        DCA
+                    </Typography>
+
+                    <LocalAtmIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+                    <Typography
+                        variant="h5"
+                        noWrap
+                        component="a"
+                        href=""
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'flex', md: 'none' },
+                            flexGrow: 1,
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        DCA
+                    </Typography>
+                    <Button color="inherit" variant="outlined">Launch our app</Button>
+
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
+};
