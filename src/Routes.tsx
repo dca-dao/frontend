e816bcfca6b6eb2e6ea15e4ba26e1b1a241beadf
Louis <louis.dutoitld@gmail.com>
2022-06-02 18:33:04 +0200

    Fix metamask

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ChainId, DAppProvider } from "@usedapp/core"
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Metamask } from './components/DCA/Metamask';
 import { ResponsiveAppBar } from './components/Landing/AppBar';
@@ -14,7 +15,15 @@ function Routes() {
                     <LandingCore />
                 </Route>
                 <Route exact path="/dca">
-                    <Metamask />
+                    <DAppProvider config={{
+                        supportedChains: [ChainId.Kovan],
+                        notifications: {
+                            expirationPeriod: 1000,
+                            checkInterval: 1000
+                        }
+                    }}>
+                        <Metamask />
+                    </DAppProvider>
                 </Route>
             </Switch>
         </Router>
